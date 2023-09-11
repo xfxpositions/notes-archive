9dd4c4a7027d0e7aa76202b153b47678b6dbea6c
xfxpositions <xfxpositions@gmail.com>
2023-09-11 03:22:42 +0300

    i don't even know what i added, but i can say i don't write rust anymore

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
+use dotenv::dotenv;
 use maria::{handler, Arc, HandlerFn, Mutex, Request, Response, Router};
 use markdown;
+use serde::{Deserialize, Serialize};
+use std::env;
 use std::ffi::OsStr;
+use std::io::{Read, Seek, Write};
 use std::path::Path;
 use std::path::PathBuf;
-use std::{fs, io};
+use std::{fs, fs::File, io};
 
 fn list_dir_entries(path: &Path) -> Result<Vec<PathBuf>, std::io::Error> {
     let entries = fs::read_dir(path)?
@@ -55,8 +59,28 @@ fn render_markdown(content: &String) -> String {
     return markdown::to_html(content);
 }
 
+// also deletes everything
+fn write_string_to_file_truncate(fpath: &Path, content: &String) -> Result<(), std::io::Error> {
+    let fpath = std::path::Path::new("./test.txt");
+    let mut file = fs::OpenOptions::new()
+        .truncate(true)
+        .write(true)
+        .open(fpath)?;
+
+    let _ = file.write(content.as_bytes())?;
+    return Ok(());
+}
+
 #[tokio::main]
 async fn main() {
+    //dotenv
+    dotenv().ok();
+
+    let _ = match env::var("fpassword") {
+        Ok(fpassword) => fpassword,
+        Err(e) => panic!(".env error {:?}", e),
+    };
+
     let mut router = Router::new();
 
     let home: HandlerFn = handler!(_req, res, {
@@ -127,9 +151,92 @@ async fn main() {
     let vir_path = format!("{}/:*fpath", PUBLIC_VIR_DIR);
     router.get(vir_path.as_str(), vec![file_handler]);
 
-    let edit_file: HandlerFn = handler!(req, res, {});
+    let edit_file: HandlerFn = handler!(req, res, {
+        let mut fpath = req.params.get("*fpath");
+        let formatted_path = format!("{}/{}", &PUBLIC_DIR, fpath.unwrap());
+        if !fpath.unwrap().contains(&PUBLIC_DIR) {
+            fpath = Some(&formatted_path);
+        }
+        println!("{:?}", fpath);
+        if fpath.is_none() {
+            res.set_status_code_raw(401);
+            res.send_html("err: please provide a file path");
+            return;
+        }
+        // check fpath is exist
+        match fs::metadata(fpath.unwrap()) {
+            Ok(_) => {}
+            Err(_) => {
+                res.send_html("err: file not found");
+                return;
+            }
+        }
+        let mut render_str = format!("<script>const fpath = \"{}\"</script>", fpath.unwrap());
+        render_str.push_str(
+            format!(
+                "<script>const fcontent = `{}`</script>",
+                fs::read_to_string(fpath.unwrap()).unwrap()
+            )
+            .as_str(),
+        );
 
-    router.post("/edit/:*fpath", vec![edit_file]);
+        match std::fs::read_to_string("./src/views/edit.html") {
+            Ok(mut html_str) => {
+                html_str = render_str + html_str.as_str();
+                res.send_html(html_str.as_str());
+            }
+            Err(e) => res.send_html(format!("Content error: {:?}", e).as_str()),
+        }
+    });
+
+    #[derive(Deserialize, Serialize, Clone)]
+    struct EditFilePost {
+        content: String,
+        password: String,
+    }
+
+    let edit_file_post: HandlerFn = handler!(req, res, {
+        let body: Result<EditFilePost, serde_json::Error> = serde_json::from_str(req.body.as_str());
+        if body.is_err() {
+            res.set_status_code_raw(400);
+            res.send_text(format!("File err: {:?}", body.err()).as_str());
+            return;
+        }
+
+        let body_unwrap = body.unwrap().clone();
+
+        // check password
+        if body_unwrap.password != env::var("fpassword").unwrap() {
+            res.set_status_code_raw(403);
+            res.send_text(format!("wrong password maan",).as_str());
+            return;
+        }
+
+        let fpath = req.params.get("*fpath");
+        if fpath.is_none() {
+            res.set_status_code_raw(401);
+            res.send_html("err: please provide a file path");
+            return;
+        }
+        //finally
+
+        match write_string_to_file_truncate(Path::new(fpath.unwrap()), &body_unwrap.content) {
+            Ok(_) => {
+                res.set_status_code_raw(200);
+                res.send_text(format!("Ok").as_str());
+                return;
+            }
+            Err(e) => {
+                res.set_status_code_raw(500);
+                res.send_text(format!("some error happened, {:?}", e).as_str());
+                return;
+            }
+        }
+    });
+
+    router.post("/edit/:*fpath", vec![edit_file_post]);
+
+    router.get("/editz/:*fpath", vec![edit_file]);
 
     println!("listenign 8080!");
     router.listen(8080).await;
